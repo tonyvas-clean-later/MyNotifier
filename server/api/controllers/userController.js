7d437638e20a3af895088eb641160b53887acb79
Antony Vasiliev <tony.vasiliev8@gmail.com>
2022-08-25 19:45:21 -0400

    Removed user id from being returned when creating user

--- server/api/controllers/userController.js
+++ server/api/controllers/userController.js
@@ -3,8 +3,8 @@ let model = require('../models/userModel');
 
 router.post('/users', (req, res) => {
     model.createUser().then(user => {
-        // Create user and send its info
-        res.status(200).json(user);
+        // Create user and send its keys
+        res.status(200).json({private: user.private, public: user.public});
     }).catch(err => {
         // If failed to create log error and send server error code
         console.error(err);
